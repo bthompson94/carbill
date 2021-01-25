804531b826ef95170029de8c7e855c047f7f8d10
bthompson94 <77295706+bthompson94@users.noreply.github.com>
2021-01-25 10:48:09 -0500

    Add files via upload

---
+++ CarbillProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; 
+
+namespace Carbill
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //Variables 
+        {
+            Console.WriteLine("Thank you for choosing Serenity Auto Repair");
+        }
+    }
+} Write("Please enter total for oil change");
+total1 = ReadLine(30);
+
+  Write("Please enter total for tires");
+total2 = ReadLine(100);
+
+  Write("Total for inspections");
+total3 = ReadLine (16);
+
+  WriteLine($"Your total is {446}");
+
+//WriteLine (Your total bill {447.06), billTotal.ToString ("C"));
+
+
+ReadKey(); 
